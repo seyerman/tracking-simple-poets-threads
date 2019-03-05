b7c62c526505fe69f99f17564cae28506eac1e61
Juan Reyes <seyerman@gmail.com>
2019-03-05 16:57:38 -0500

    Version B of a thread tracking program example

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,27 +1,25 @@
 package ui;
-
 import model.Poem;
 import threads.PoemThread;
 
 public class Main {
-	public static void main(String[] args) {
-		Poem p1 = new Poem( "There is another sky",
-							"Emily Dickinson",
-							"Ever serene and fair\n"+
-							"And there is another sunshine\n"+
-							"Though it be darkness there");
+	public static void main(String[] args) throws InterruptedException {
+		Poem p1 = new Poem( "When You Come",
+							"Maya Angelou",
+							"When you come to me, unbidden\n" + 
+							"Beckoning me\n" + 
+							"To long-ago rooms\n" + 
+							"Where memories lie.");
 		PoemThread pt1 = new PoemThread("p1",p1);
-		
-		Poem p2 = new Poem( "All the World's a Stage",
-				"William Shakespeare, Bard of Avon",
-				"And all the men and women merely player\n"+
-				"They have their exits and their entrances");
+		Poem p2 = new Poem( "It is Here",
+				"Harold Pinter",
+				"What sound was that?\n" + 
+				"I turn away, into the shaking room.");
 		PoemThread pt2 = new PoemThread("p2",p2);
-		
 		pt1.start();
 		pt2.start();
-		
-		System.out.println("The algorithms are poetry!");
+		pt1.join();
+		System.out.println("The algorithms are poetry?");
 	}
 }
 
